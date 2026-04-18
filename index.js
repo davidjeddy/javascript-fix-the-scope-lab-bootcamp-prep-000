8e16375e92cca2322891562a205bf14ae44152a3
David Eddy <me@davidjeddy.com>
2026-04-18 12:29:07 +0200

    chore: archive project — history cleared, redirecting to Codeberg
    This repository has been archived. The project has moved to:
    https://codeberg.org/DavidJEddy/javascript-fix-the-scope-lab-bootcamp-prep-000

---
+++ index.js
@@ -0,0 +1,33 @@
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  var animal = animal;
+  if (animal !== 'dog') {
+    animal = 'cat';
+  }
+
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  return animal
+}
+
+function add2(n) {
+  // Feel free to move things around!
+  const two = 2
+
+  return n + two
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
+var theFunk = funkyFunction()()
